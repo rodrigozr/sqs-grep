67a5e2b91f3013ed814f73e6289e9652200ca206
Rodrigo Zechin Rosauro <rodrigo.rosauro@gmail.com>
2020-03-25 11:23:24 -0300

    Simplify 'nyc' usage for code coverage reporting

--- test.sh
+++ test.sh
@@ -6,10 +6,6 @@ src/main.js --version || exit 1
 # Run the tests with code coverage
 ./node_modules/.bin/nyc ./node_modules/mocha/bin/mocha || exit 1
 
-# Generate the coverage report
-mkdir ./coverage
-./node_modules/.bin/nyc report --reporter=text-lcov > ./coverage/lcov.info || exit 1
-
 # When running on Travis, also upload the coverate results to coveralls
 [ "$TRAVIS_JOB_ID" = "" ] || cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js || exit 1
 
